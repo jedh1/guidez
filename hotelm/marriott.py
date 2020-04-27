358cb9fdcc08bcb762120df2203038e241167fae
Jed <jedhcl@gmail.com>
2020-04-27 14:21:11 -0700

    added chrome driver window size and disable gpu

--- hotelm/marriott.py
+++ hotelm/marriott.py
@@ -25,6 +25,8 @@ def prepare_driver(url):
     chrome_options = webdriver.ChromeOptions()
     chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     chrome_options.add_argument("--headless")
+    chrome_options.add_argument('--window-size=1920,1080')
+    chrome_options.add_argument('--disable-gpu')
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--no-sandbox")
     driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)

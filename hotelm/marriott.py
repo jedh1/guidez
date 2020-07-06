22522843b838e2cd3cf922927fec32cc50167430
Jed <jedhcl@gmail.com>
2020-07-06 16:31:21 -0700

    4

--- hotelm/marriott.py
+++ hotelm/marriott.py
@@ -25,7 +25,7 @@ def prepare_driver(url):
     chrome_options = webdriver.ChromeOptions()
     chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     chrome_options.add_argument("--headless")
-    # chrome_options.add_argument('--window-size=1400,800')
+    chrome_options.add_argument('--window-size=1400,800')
     chrome_options.add_argument('--disable-gpu')
     chrome_options.add_argument("--no-sandbox")
     driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
